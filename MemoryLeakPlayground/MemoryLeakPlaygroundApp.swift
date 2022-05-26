9977ec04cdf7de3b06acd580a5a42b1e415f4995
Kuniwak <yuki.kokubun@dena.com>
2022-05-26 15:24:27 +0900

    Initial Commit

---
+++ MemoryLeakPlayground/MemoryLeakPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLeakPlaygroundApp.swift
+//  MemoryLeakPlayground
+//
+//  Created by Yuki Kokubun on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLeakPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
