a07a69a7a2380ed903e5ddf705fbe79bf9e1334e
Kuniwak <yuki.kokubun@dena.com>
2022-10-20 12:59:57 +0900

    Fix weight size

---
+++ MemoryLeakPlayground/Weight1.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+
+class Weight1 {
+    private var data: [UInt64] = Array(repeating: 0, count: 1024)
+}
--- MemoryLeakPlayground/Weight2.m
+++ MemoryLeakPlayground/Weight2.m
@@ -4,13 +4,13 @@
 @implementation Weight2
 - (id)init {
    if (self = [super init]) {
-      size_t size10MiB = 10*1024*1024;
-      void* mem = malloc(size10MiB);
+      size_t size8MiB = 8*1024*1024;
+      void* mem = malloc(size8MiB);
       if (mem == NULL) {
          return self;
       }
       // NOTE: Avoid memory overcommit.
-      memset(mem, size10MiB, 1);
+      memset(mem, size8MiB, 1);
       _mem = mem;
    }
    return self;
